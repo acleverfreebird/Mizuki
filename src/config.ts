a328625378f7abb4eda45db842f072686988a961
acleverfreebird <2280136516@qq.com>
2026-02-23 18:19:35 +0800

    try to use edgeone to make it quickly

--- src/config.ts
+++ src/config.ts
@@ -119,22 +119,22 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-				"/assets/desktop-banner/banner_1.jpg",
-				"/assets/desktop-banner/banner_2.jpg",
-				"/assets/desktop-banner/banner_3.jpg",
-				"/assets/desktop-banner/banner_4.jpg",
-				"/assets/desktop-banner/banner_5.jpg",
-				"/assets/desktop-banner/banner_6.jpg",
-				"/assets/desktop-banner/banner_7.jpg",
+				"https://pic.freebird2913.tech/banner_1.jpg",
+				"https://pic.freebird2913.tech/banner_2.jpg",
+				"https://pic.freebird2913.tech/banner_3.jpg",
+				"https://pic.freebird2913.tech/banner_4.jpg",
+				"https://pic.freebird2913.tech/banner_5.jpg",
+				"https://pic.freebird2913.tech/banner_6.jpg",
+				"https://pic.freebird2913.tech/banner_7.jpg",
 			], // 桌面横幅图片
 			mobile: [
-				"/assets/desktop-banner/banner_1.jpg",
-				"/assets/desktop-banner/banner_2.jpg",
-				"/assets/desktop-banner/banner_3.jpg",
-				"/assets/desktop-banner/banner_4.jpg",
-				"/assets/desktop-banner/banner_5.jpg",
-				"/assets/desktop-banner/banner_6.jpg",
-				"/assets/desktop-banner/banner_7.jpg",
+				"https://pic.freebird2913.tech/banner_1.jpg",
+				"https://pic.freebird2913.tech/banner_2.jpg",
+				"https://pic.freebird2913.tech/banner_3.jpg",
+				"https://pic.freebird2913.tech/banner_4.jpg",
+				"https://pic.freebird2913.tech/banner_5.jpg",
+				"https://pic.freebird2913.tech/banner_6.jpg",
+				"https://pic.freebird2913.tech/banner_7.jpg",
 			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
@@ -227,22 +227,22 @@ export const siteConfig: SiteConfig = {
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	src: {
 		desktop: [
-			"/assets/desktop-banner/banner_1.jpg",
-			"/assets/desktop-banner/banner_2.jpg",
-			"/assets/desktop-banner/banner_3.jpg",
-			"/assets/desktop-banner/banner_4.jpg",
-			"/assets/desktop-banner/banner_5.jpg",
-			"/assets/desktop-banner/banner_6.jpg",
-			"/assets/desktop-banner/banner_7.jpg",
+			"https://pic.freebird2913.tech/banner_1.jpg",
+			"https://pic.freebird2913.tech/banner_2.jpg",
+			"https://pic.freebird2913.tech/banner_3.jpg",
+			"https://pic.freebird2913.tech/banner_4.jpg",
+			"https://pic.freebird2913.tech/banner_5.jpg",
+			"https://pic.freebird2913.tech/banner_6.jpg",
+			"https://pic.freebird2913.tech/banner_7.jpg",
 		], // 桌面横幅图片
 		mobile: [
-			"/assets/desktop-banner/banner_1.jpg",
-			"/assets/desktop-banner/banner_2.jpg",
-			"/assets/desktop-banner/banner_3.jpg",
-			"/assets/desktop-banner/banner_4.jpg",
-			"/assets/desktop-banner/banner_5.jpg",
-			"/assets/desktop-banner/banner_6.jpg",
-			"/assets/desktop-banner/banner_7.jpg",
+			"https://pic.freebird2913.tech/banner_1.jpg",
+			"https://pic.freebird2913.tech/banner_2.jpg",
+			"https://pic.freebird2913.tech/banner_3.jpg",
+			"https://pic.freebird2913.tech/banner_4.jpg",
+			"https://pic.freebird2913.tech/banner_5.jpg",
+			"https://pic.freebird2913.tech/banner_6.jpg",
+			"https://pic.freebird2913.tech/banner_7.jpg",
 		], // 移动横幅图片
 	}, // 使用本地横幅图片
 	position: "center", // 壁纸位置，等同于 object-position
