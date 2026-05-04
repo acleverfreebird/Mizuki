2d70d313380a2d0a25345a51ebc5afd6778ed4bb
acleverfreebird <2280136516@qq.com>
2026-05-04 13:50:45 +0800

    fix comment

--- src/config.ts
+++ src/config.ts
@@ -416,7 +416,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 };
 
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
 		envId: "https://twikoojs.freebird2913.tech",
